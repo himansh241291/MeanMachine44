7c7cdfc53d33b318d0d00dddaa1604955d6e8668
himansh241291 <himansh241291@gmail.com>
2026-09-15 17:49:04 +0530

    Remove undefined support detector tests

--- tests/test_setup.py
+++
@@ -1,7 +0,0 @@
-from meanmachine44.candles import Candle
-from meanmachine44.setup import ma44_support
-
-
-def test_ma44_support():
-    assert ma44_support(Candle(101, 105, 99, 104), 100)
-    assert not ma44_support(Candle(102, 105, 101, 104), 100)
