6de1e96367cd7ddfdc683bd0511e4e3a6e8ef942
himansh241291 <himansh241291@gmail.com>
2026-09-15 17:50:01 +0530

    Expand ATR input validation tests

--- tests/test_atr.py
+++ tests/test_atr.py
@@ -1,3 +1,5 @@
+import math
+
 import pytest
 
 from meanmachine44.atr import (
@@ -20,6 +22,12 @@ def test_true_range_rejects_inverted_range():
         true_range(99, 100, 105)
 
 
+@pytest.mark.parametrize("value", [math.nan, math.inf, -math.inf])
+def test_true_range_rejects_non_finite(value):
+    with pytest.raises(ValueError):
+        true_range(value, 100, 105)
+
+
 def test_atr_sma():
     assert atr_sma([1, 2, 3], 2) == [None, 1.5, 2.5]
 
@@ -27,6 +35,12 @@ def test_atr_sma():
 def test_atr_sma_rejects_negative_true_range():
     with pytest.raises(ValueError):
         atr_sma([1, -1, 3], 2)
+
+
+@pytest.mark.parametrize("value", [math.nan, math.inf, -math.inf])
+def test_atr_sma_rejects_non_finite_true_range(value):
+    with pytest.raises(ValueError):
+        atr_sma([1, value, 3], 2)
 
 
 def test_atr14_sma_uses_ohlc():
