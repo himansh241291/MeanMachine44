bb124f4e9f850bde301472bfe4a2cb61ede1faeb
himansh241291 <himansh241291@gmail.com>
2026-09-16 12:28:13 +0530

    Add weekly daily MA44 scan gate

--- src/meanmachine44/scan.py
+++ src/meanmachine44/scan.py
@@ -4,3 +4,9 @@ from .ma44 import rising
 
 def rising_ma44(closes: list[float]) -> list[bool]:
     return rising(sma44(closes), 3)
+
+
+def passes_ma44_scan(weekly_closes: list[float], daily_closes: list[float]) -> bool:
+    weekly = rising_ma44(weekly_closes)
+    daily = rising_ma44(daily_closes)
+    return bool(weekly and weekly[-1] and daily and daily[-1])
